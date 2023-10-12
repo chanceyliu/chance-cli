4f9c6c484b21cf40f17f11965bb913f01203df86
chanceyliu <1445885764@qq.com>
2023-10-12 17:06:08 +0800

    feat: 完善脚手架

--- packages/create/lib/index.js
+++ packages/create/lib/index.js
@@ -9,35 +9,68 @@ const colors_1 = __importDefault(require("colors"));
 const download_git_repo_1 = __importDefault(require("download-git-repo"));
 const signale_1 = __importDefault(require("signale"));
 const node_fs_1 = __importDefault(require("node:fs"));
+const inquirer_1 = __importDefault(require("inquirer"));
+const node_child_process_1 = require("node:child_process");
+const node_path_1 = __importDefault(require("node:path"));
 let proName = "";
+// 当前路径
+let dest;
 commander_1.program.arguments("[name]").action((name) => {
     proName = name;
+    dest = node_path_1.default.resolve(process.cwd(), proName);
 });
 commander_1.program.parse(process.argv);
-if (!proName) {
-    signale_1.default.warn(`请填写项目名称: ${colors_1.default.green("create-app <project-directory>")}`);
-    process.exit(1);
-}
-if (node_fs_1.default.existsSync(proName)) {
-    signale_1.default.warn(`${proName} 项目已存在，请检查`);
-    process.exit(1);
-}
-else {
-    signale_1.default.pending("创建中");
-    (0, download_git_repo_1.default)("chanceyliu/basic-react-template", proName, (err) => {
-        if (err) {
-            signale_1.default.error(err);
-        }
-        else {
-            const fileRoad = `${proName}/package.json`;
-            let oldContent = node_fs_1.default.readFileSync(fileRoad).toString();
-            let newContent = {
-                ...JSON.parse(oldContent),
-                name: proName,
-                version: '1.0.0'
-            };
-            node_fs_1.default.writeFileSync(fileRoad, JSON.stringify(newContent, null, '\t'));
-            signale_1.default.success(`${proName} 创建成功`);
-        }
-    });
-}
+const run = async () => {
+    if (!proName) {
+        signale_1.default.warn(`请填写项目名称: ${colors_1.default.green("create-app <project-directory>")}`);
+        process.exit(1);
+    }
+    if (node_fs_1.default.existsSync(proName)) {
+        signale_1.default.warn(`${proName} 项目已存在，请检查`);
+        process.exit(1);
+    }
+    else {
+        signale_1.default.pending("创建中");
+        await (0, download_git_repo_1.default)("chanceyliu/basic-react-template", proName, (err) => {
+            if (err) {
+                signale_1.default.error(err);
+            }
+            else {
+                const fileRoad = `${proName}/package.json`;
+                let oldContent = node_fs_1.default.readFileSync(fileRoad).toString();
+                let newContent = {
+                    ...JSON.parse(oldContent),
+                    name: proName,
+                    version: "1.0.0",
+                };
+                node_fs_1.default.writeFileSync(fileRoad, JSON.stringify(newContent, null, "\t"));
+            }
+        });
+        await inquirer_1.default
+            .prompt([
+            {
+                type: "confirm",
+                name: "install",
+                message: "是否安装依赖",
+            },
+        ])
+            .then((res) => {
+            if (res.install) {
+                signale_1.default.pending("Installing 📦 \n");
+                const isWin32 = process.platform === "win32";
+                const subProcess = (0, node_child_process_1.spawnSync)(isWin32 ? "npm.cmd" : "npm", ["install", "--legacy-peer-deps"], {
+                    stdio: "inherit",
+                    cwd: dest,
+                });
+                if (subProcess.status === 1) {
+                    signale_1.default.error("😕  安装依赖出错, 请自行安装依赖");
+                }
+                else {
+                    signale_1.default.success("安装完成 👨‍💻‍!!!");
+                }
+            }
+        });
+        signale_1.default.success(`${proName} 创建成功`);
+    }
+};
+run();
--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -5,46 +5,83 @@ import colors from "colors";
 import download from "download-git-repo";
 import signale from "signale";
 import fs from "node:fs";
+import inquirer from "inquirer";
+import { spawnSync } from "node:child_process";
+import path from "node:path";
 
 let proName = "";
+// 当前路径
+let dest: string;
 
 program.arguments("[name]").action((name) => {
   proName = name;
+  dest = path.resolve(process.cwd(), proName);
 });
 
 program.parse(process.argv);
 
-if (!proName) {
-  signale.warn(
-    `请填写项目名称: ${colors.green("create-app <project-directory>")}`
-  );
-  process.exit(1);
-}
+const run = async () => {
+  if (!proName) {
+    signale.warn(
+      `请填写项目名称: ${colors.green("create-app <project-directory>")}`
+    );
+    process.exit(1);
+  }
 
-if (fs.existsSync(proName)) {
-  signale.warn(`${proName} 项目已存在，请检查`);
-  process.exit(1);
+  if (fs.existsSync(proName)) {
+    signale.warn(`${proName} 项目已存在，请检查`);
+    process.exit(1);
+  } else {
+    signale.pending("创建中");
 
-} else {
-  signale.pending("创建中");
-  download("chanceyliu/basic-react-template", proName, (err: any) => {
-    if (err) {
-      signale.error(err);
-    } else {
+    await download("chanceyliu/basic-react-template", proName, (err: any) => {
+      if (err) {
+        signale.error(err);
+      } else {
+        const fileRoad = `${proName}/package.json`;
+        let oldContent = fs.readFileSync(fileRoad).toString();
+        let newContent = {
+          ...JSON.parse(oldContent),
+          name: proName,
+          version: "1.0.0",
+        };
 
-      const fileRoad = `${proName}/package.json`
-      let oldContent = fs.readFileSync(fileRoad).toString()
-      let newContent = {
-        ...JSON.parse(oldContent),
-        name: proName,
-        version: '1.0.0'
+        fs.writeFileSync(fileRoad, JSON.stringify(newContent, null, "\t"));
       }
+    });
 
-      fs.writeFileSync(fileRoad, JSON.stringify(newContent, null, '\t'))
+    await inquirer
+      .prompt([
+        {
+          type: "confirm",
+          name: "install",
+          message: "是否安装依赖",
+        },
+      ])
+      .then((res) => {
+        if (res.install) {
+          signale.pending("Installing 📦 \n");
 
-      signale.success(`${proName} 创建成功`);
-    }
-  });
-}
+          const isWin32 = process.platform === "win32";
+          const subProcess = spawnSync(
+            isWin32 ? "npm.cmd" : "npm",
+            ["install", "--legacy-peer-deps"],
+            {
+              stdio: "inherit",
+              cwd: dest,
+            }
+          );
 
+          if (subProcess.status === 1) {
+            signale.error("😕  安装依赖出错, 请自行安装依赖");
+          } else {
+            signale.success("安装完成 👨‍💻‍!!!");
+          }
+        }
+      });
 
+    signale.success(`${proName} 创建成功`);
+  }
+};
+
+run();
